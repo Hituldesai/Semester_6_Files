b9422592dddad1503a3611f36a1883fab3604eaf
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2020-01-18 23:24:31 +0530

    Version 001.07.001 20200118

---
+++ SC635/Labwork/Lab_1/catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/aaronjs/Files/Repositories_and_Directories/Semester_6_Files/SC635/Labwork/Lab_1/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
