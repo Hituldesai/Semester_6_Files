d0527a16e59037ef0fbde78e21438ceb156b18a6
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2020-02-05 18:20:57 +0530

    Version 001.15.002 20200205

--- EE324/Labwork/Lab_1/Lab_1.ino
+++ EE324/Labwork/Lab_1/Lab_1.ino
@@ -1,10 +1,10 @@
-double kp = 1;
-double kd = 1;
-double ki = 1;
+double K_p = 200;
+double K_d = 500;
+double K_i = 0.002;
 
-const int pinH = 2;
-const int pinL = 3;
-const int enable = 31;
+const int pinH   = 2;
+const int pinL   = 3;
+const int enable = 13;
 
 double refValue;
 double currValue;
@@ -18,54 +18,82 @@ unsigned long prevTime;
 unsigned long currTime;
 
 void setup() {
-  // put your setup code here, to run once:
   refValue = analogRead(A0);
-  
-  refValue += 512;
-  if(refValue > 1023){
-    refValue -= 1024;    
-  }
-  
-  pinMode(pinH,OUTPUT);
-  pinMode(pinL,OUTPUT);
-  pinMode(enable,OUTPUT);
 
-  analogWrite(pinH,0);
-  analogWrite(pinL,0);
-  digitalWrite(enable,HIGH);
+  refValue += 512;
+//  refValue += 10.0/360.0 * 1024.0;
+  //refValue += 10/360.0*1024;
+  
+  if(refValue > 1023){
+    refValue -= 1024;
+  }
+  refValue = refValue * 360 / 1024.0;
+  refValue = refValue - 30;
+  if(refValue > 360){
+    refValue -= 360;
+  }
+  if(refValue < 0){
+    refValue += 360;
+  }
+//   refValue=(refValue-30)*360/350.0;
+   
+  if(refValue > 90 && refValue < 270){
+    refValue +=10;
+  }
+  else{
+    refValue -= 10;
+  }
+  //refValue += 10;
+  pinMode(pinH,   OUTPUT);
+  pinMode(pinH,   OUTPUT);
+  pinMode(enable, OUTPUT);
+
+  analogWrite(pinH, 0);
+  analogWrite(pinL, 0);
+  digitalWrite(enable, HIGH);
 
   currTime = millis();
   prevTime = currTime;
-  
+
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   currValue = analogRead(A0);
-  error = currValue - refValue;
-  if(error > 511){
-    error -= 1024;
+  currValue = currValue * 360 / 1024.0;
+    currValue = currValue - 30;
+  if(currValue > 360){
+    currValue -= 360;
+  }
+  if(currValue < 0){
+    currValue += 360;
+  }
+  currTime = millis();
+  error     = currValue - refValue;
+  if(error > 180){
+    error -= 360;
+  }
+  else if(error < -180){
+    error += 360;
   }
   
-  currTime = millis();
   dt = currTime - prevTime;
-  prevTime = currTime;
-  
-  integral += error * dt;
+  integral  += error * dt;
   derivative = (error - prevError) / dt;
-  prevError = error;
-  
-  control = (kp * error + kd * derivative + ki * integral)/4;
-
-//  delay(100);
-
+  prevError  = error;
+  if(abs(error) < 3.6){
+    K_p = 5;
+    K_i = 0;}
+  control    = (K_p * error + K_d * derivative + K_i * integral) ;
   if(control > 0){
-    analogWrite(pinH,min(abs(control),255));
-    analogWrite(pinL,0);
+    analogWrite(pinH, min(abs(control), 255));
+    analogWrite(pinL, 0);
   }
   else{
-    analogWrite(pinL,min(abs(control),255));
-    analogWrite(pinH,0);
+    analogWrite(pinL, min(abs(control), 255));
+    analogWrite(pinH, 0);
   }
+  prevTime = currTime;
+  //Serial.print(error);Serial.print(" ");Serial.print(K_p * error);Serial.print(" ");Serial.print(K_d * derivative);Serial.print(" ");Serial.println(K_i * integral);
+  Serial.print(error);Serial.print(" ");Serial.print(currTime);Serial.println(";");
 }
